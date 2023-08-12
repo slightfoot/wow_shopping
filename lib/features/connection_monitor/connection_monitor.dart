466c1f14acc8675b57fe23b64719441e2deac130
Simon Lightfoot <simon@devangels.london>
2023-08-12 18:07:24 +0100

    Update connectivity monitor to use custom layout.

--- lib/features/connection_monitor/connection_monitor.dart
+++ lib/features/connection_monitor/connection_monitor.dart
@@ -33,39 +33,126 @@ class _ConnectionMonitorState extends State<ConnectionMonitor> {
           stream: onConnectivityChanged,
           builder: (BuildContext context, AsyncSnapshot<ConnectivityResult> snapshot) {
             final result = snapshot.requireData;
-            return Stack(
-              children: [
-                widget.child,
-                Positioned(
-                  bottom: 0.0,
-                  left: 0.0,
-                  right: 0.0,
-                  child: AnimatedSize(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.fastOutSlowIn,
-                    alignment: Alignment.topCenter,
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      heightFactor: result != ConnectivityResult.none ? 0.0 : 1.0,
-                      child: Material(
-                        color: Colors.red,
-                        child: Padding(
-                          padding: verticalPadding4 + horizontalPadding12,
-                          child: const Text(
-                            'Please check your internet connection',
-                            style: TextStyle(color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
+            return _ConnectivityBannerHost(
+              isConnected: result != ConnectivityResult.none,
+              banner: Material(
+                color: Colors.red,
+                child: Padding(
+                  padding: verticalPadding4 + horizontalPadding12,
+                  child: const Text(
+                    'Please check your internet connection',
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ],
+              ),
+              child: widget.child,
             );
           },
         );
       },
     );
+  }
+}
+
+@immutable
+class _ConnectivityBannerHost extends StatefulWidget {
+  const _ConnectivityBannerHost({
+    required this.isConnected,
+    required this.banner,
+    required this.child,
+  });
+
+  final bool isConnected;
+  final Widget banner;
+  final Widget child;
+
+  @override
+  State<_ConnectivityBannerHost> createState() => _ConnectivityBannerHostState();
+}
+
+class _ConnectivityBannerHostState extends State<_ConnectivityBannerHost>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      value: widget.isConnected ? 0.0 : 1.0,
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant _ConnectivityBannerHost oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.isConnected != widget.isConnected) {
+      if (widget.isConnected) {
+        _controller.reverse();
+      } else {
+        _controller.forward();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomMultiChildLayout(
+      delegate: _ConnectivityBannerHostDelegate(_animation),
+      children: [
+        LayoutId(
+          id: _ConnectivityBannerHostWidgetId.child,
+          child: widget.child,
+        ),
+        LayoutId(
+          id: _ConnectivityBannerHostWidgetId.banner,
+          child: widget.banner,
+        ),
+      ],
+    );
+  }
+}
+
+enum _ConnectivityBannerHostWidgetId { child, banner }
+
+class _ConnectivityBannerHostDelegate extends MultiChildLayoutDelegate {
+  _ConnectivityBannerHostDelegate(this._animation) : super(relayout: _animation);
+
+  final Animation<double> _animation;
+
+  @override
+  void performLayout(Size size) {
+    layoutChild(_ConnectivityBannerHostWidgetId.child, BoxConstraints.tight(size));
+    positionChild(_ConnectivityBannerHostWidgetId.child, Offset.zero);
+
+    final bannerSize = layoutChild(
+      _ConnectivityBannerHostWidgetId.banner,
+      BoxConstraints.tightFor(width: size.width),
+    );
+    positionChild(
+      _ConnectivityBannerHostWidgetId.banner,
+      Offset(
+        0.0,
+        size.height - (_animation.value * bannerSize.height),
+      ),
+    );
+  }
+
+  @override
+  bool shouldRelayout(covariant _ConnectivityBannerHostDelegate oldDelegate) {
+    return _animation != oldDelegate._animation;
   }
 }
